5535a18d07ae9ec0cf38bffa21739c6b5bf77253
Jack Sie <jetswayss@gmail.com>
2014-09-23 14:17:08 +0800

    version 0.1-bets

---
+++ README.md
@@ -0,0 +1,68 @@
+OpendID-Consumer
+=========
+
+Simple OpendID Consumer for NCU
+
+  - Simple
+  - Only for NCU
+  - Easy to Use
+
+Version
+----
+
+0.1-beta
+
+
+How to use
+--------------
+
+
+##### setting files
+
+* src/main/resources/openidSetting.properties
+* src/main/resources/openidCheck.properties
+
+you only have to edit openidSetting.properties with following properties
+
+```sh
+openid.return_to=http://140.115.3.97/manage/auth
+openid.realm=http://140.115.3.97
+```
+
+you have to change them to your own datas
+
+
+
+
+
+##### Use it in programming
+
+first you have to create a instance of OpenIDManager
+```sh
+OpenIDManager openIDManager = new OpenIDManager();
+```
+
+second you have to get the URL used for being clicked or redirected by user
+```sh
+String url=openIDManager.getURLString();
+```
+
+after you revice the request from user which is redirected from portal
+you can simply put the request parameters' map into the function
+```sh
+Map map=request.getParameterMap();
+boolean isCorrect=openIDManager.checkAuthentication(map);
+```
+
+if respone is true, you can get user's data
+```sh
+Map map=request.getParameterMap();
+String id=openIDManager.getStudentID(map);
+```
+
+
+
+License
+----
+
+Apache2.0
--- src/main/java/tw/edu/ncu/cc/manage/openid/OpenIDSetting.java
+++ src/main/java/tw/edu/ncu/cc/manage/openid/OpenIDSetting.java
@@ -8,44 +8,56 @@ import java.util.Properties;
 import java.util.Set;
 
 public class OpenIDSetting {
-	private Properties prop;
-	public OpenIDSetting(Properties properties) {
-		this.prop=properties;
-	}
-	
-	public URL getURL(){
-		String endpoint = getURLString();
-		URL url=null;
-		try {
-			url=new URL(endpoint);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		return url;
-	}
-	public String getURLString(){
-		String endpoint = prop.getProperty("openid.op_endpoint")+"?";
-		Set<String> names=prop.stringPropertyNames();
-		String[] keys=names.toArray(new String[names.size()]);
-		return addParameters(endpoint, keys);
-	}
+    private Properties prop;
 
-	private String addParameters(String url,String parameters[]){
-		try {
-			for(int i=0;i<parameters.length-1;i++){			
-				url+= parameters[i]+ "=" + URLEncoder.encode(prop.getProperty(parameters[i]),"UTF-8")+"&";		
-				url+= parameters[parameters.length-1]+ "=" + URLEncoder.encode(prop.getProperty(parameters[parameters.length-1]),"UTF-8");			
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return url;
-	}
-	public Properties getProp() {
-		return prop;
-	}
+    public OpenIDSetting(Properties properties) {
+        this.prop = properties;
+    }
 
-	public void setProp(Properties prop) {
-		this.prop = prop;
-	}
+    public URL getURL() {
+        String endpoint = getURLString();
+        URL url = null;
+        try {
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public String getURLString() {
+        String endpoint = prop.getProperty("openid.op_endpoint") + "?";
+        Set<String> names = prop.stringPropertyNames();
+        String[] keys = names.toArray(new String[names.size()]);
+        return addParameters(endpoint, keys);
+    }
+
+    private String addParameters(String url, String[] parameters) {
+        String resultUrl = url;
+        try {
+            for (int i = 0; i < parameters.length - 1; i++) {
+                resultUrl += parameters[i]
+                        + "="
+                        + URLEncoder.encode(prop.getProperty(parameters[i]),
+                                "UTF-8") + "&";
+                resultUrl += parameters[parameters.length - 1]
+                        + "="
+                        + URLEncoder
+                                .encode(prop
+                                        .getProperty(parameters[parameters.length - 1]),
+                                        "UTF-8");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return resultUrl;
+    }
+
+    public Properties getProp() {
+        return prop;
+    }
+
+    public void setProp(Properties prop) {
+        this.prop = prop;
+    }
 }
