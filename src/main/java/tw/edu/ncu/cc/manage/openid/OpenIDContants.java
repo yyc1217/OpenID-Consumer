e1cc8a14ad8fac13d2006719174ae11631c46a58
jasonChiu <jason1122g@gmail.com>
2015-01-13 21:31:32 +0800

    add open id constants

---
+++ src/main/java/tw/edu/ncu/cc/manage/openid/OpenIDContants.java
@@ -0,0 +1,17 @@
+package tw.edu.ncu.cc.manage.openid;
+
+public class OpenIDContants {
+
+    public static final String CHARSET = "UTF-8";
+
+    public static final String CORRECT_RESPONSE = "is_valid:true";
+
+    public static final String NAME_SPACE = "openid.ns";
+    public static final String END_POINT = "openid.op_endpoint";
+    public static final String IDENTITY = "openid.identity";
+    public static final String REALM = "openid.realm";
+    public static final String MODE = "openid.mode";
+
+    public static final String CHECK_AUTHENTICATION = "check_authentication";
+
+}
